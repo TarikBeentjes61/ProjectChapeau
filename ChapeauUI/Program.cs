ddb3b9245a452cd1245931255c1579c07af78d46
Luciano010 <693538@student.inholland.nl>
2023-06-05 12:30:00 +0200

    test

--- ChapeauUI/Program.cs
+++ ChapeauUI/Program.cs
@@ -12,7 +12,6 @@ namespace ChapeauUI
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new CreateOrderForm());
-            // test
         }
     }
 }
